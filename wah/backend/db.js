d7b2f13b5c560edad88b8bffa2fdc0baabcf9439
Sathvik Narala <sathviknarala2003@gmail.com>
2023-04-14 19:19:04 +0530

    Installed nodemon

--- wah/backend/db.js
+++ wah/backend/db.js
@@ -1,16 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://sathviknarala2003:Wah123@cluster0.cowo7sp.mongodb.net/?retryWrites=true&w=majority'
-const mongoDB = async() =>{
-    await mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected Successfully');
-        // const fetched_data = mongoose.connection.db.collection("food_items");
-        // fetched_data.find({}).toArray(function(err, data){
-        //     if(err) console.log(err);
-        //     else console.log(data);
-        // });
-    })
-    .catch((err) => console.error(err));
+const mongoURI = 'mongodb+srv://wah:Wah123@cluster0.cowo7sp.mongodb.net/?retrywrites=true&w=majority'
+const mongoDB = ()=>{  
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected Successfully");
+    });
 }
 
-module.exports = mongoDB;
+
+module.exports = mongoDB
--- wah/backend/index.js
+++ wah/backend/index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 4000
-const mongoDB = require("./db")
-
-mongoDB()
+const mongoDB = require('./db')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Backend app listening on port ${port}`)
+  console.log(`Wah app listening on port ${port}`)
 })
