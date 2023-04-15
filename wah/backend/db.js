a038508b181bb8e88136cf5ab78ed097b95e314c
Sathvik Narala <sathviknarala2003@gmail.com>
2023-04-15 13:00:07 +0530

    user check done

--- wah/backend/db.js
+++ wah/backend/db.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://wah:Wah123@cluster0.cowo7sp.mongodb.net/?retrywrites=true&w=majority'
-const mongoDB = ()=>{  
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected Successfully");
-    });
+const mongoURI = "mongodb+srv://sathviknarala2003:Wah123@cluster0.cowo7sp.mongodb.net/wah?retryWrites=true&w=majority"
+const mongoDB = async()=>{  
+    await mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(async()=> {
+        console.log('Connected Successfully');
+        const fetched_data = await mongoose.connection.db.collection("food_items");
+        fetched_data.find().toArray(function(err, data){
+            if(err) console.log(err);
+            else console.log(data);
+        });
+    })
+    .catch((err) => console.error(err));
 }
 
 
