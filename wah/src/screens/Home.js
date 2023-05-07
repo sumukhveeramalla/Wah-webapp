a0cb9e7a57ff230df19d075b6b5d631970b05a69
Sathvik Narala <sathviknarala2003@gmail.com>
2023-05-07 18:12:43 +0530

    Populated food Data

--- wah/src/screens/Home.js
+++ wah/src/screens/Home.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card';
 import Carousel from '../components/Carousel';
 
 export default function Home() {
+
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async()=>{
+    let response = await fetch("http://localhost:4000/api/foodData", {
+      method:'POST',
+      headers:{
+        'Content-Type' : 'application/json'
+      }
+    });
+
+    response = await response.json();
+    console.log(response[0], response[1]);
+
+    setFoodItem(response[0]);
+    setFoodCat(response[1]);
+
+  }
+
+  useEffect(()=>{
+    loadData()
+  }, [])
+
   return (
     <div>
       <div>
@@ -13,12 +37,14 @@ export default function Home() {
       <div>
         <Carousel/>
       </div>
-      <div className='m-3'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div className='container'>
+        {
+          foodCat !== [] ? foodCat.map((data)=>{
+            return(
+              <div key={data._id}>{data.CategoryName}</div>
+            )
+          }): <div>""""""""""</div>
+        }
         <Card/>
       </div>
       <div>
